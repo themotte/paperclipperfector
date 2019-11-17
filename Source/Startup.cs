3c2ca76f2ca52e631c63c356801f9a8a08ecff42
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2019-11-17 10:08:20 -0600

    Add approved/rejected pages, clean up the last remnants of the example project.

--- Source/Startup.cs
+++ Source/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PaperclipPerfector.Data;
 
 namespace PaperclipPerfector
 {
@@ -28,7 +27,6 @@ namespace PaperclipPerfector
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
